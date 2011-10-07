4b6453f754b2d4026e7a48acbdc4e0bf2a5b8ef2
Luke Dashjr <luke-jr+git@utopios.org>
2011-10-07 16:47:39 -0400

    Python 3 compatibility (2.7 still supported)

--- base58.py
+++ base58.py
@@ -4,6 +4,13 @@ import hashlib
 class SHA256:
 	new = hashlib.sha256
 
+if str != bytes:
+	# Python 3.x
+	def ord(c):
+		return c
+	def chr(n):
+		return bytes( (n,) )
+
 ### BEGIN: Copied from https://bitcointalk.org/index.php?topic=1026.0 (public domain)
 __b58chars = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
 __b58base = len(__b58chars)
@@ -12,7 +19,7 @@ def b58encode(v):
   """ encode v, which is a string of bytes, to base58.
   """
 
-  long_value = 0L
+  long_value = 0
   for (i, c) in enumerate(v[::-1]):
     long_value += (256**i) * ord(c)
 
@@ -35,11 +42,11 @@ def b58encode(v):
 def b58decode(v, length):
   """ decode v into a string of len bytes
   """
-  long_value = 0L
+  long_value = 0
   for (i, c) in enumerate(v[::-1]):
     long_value += __b58chars.find(c) * (__b58base**i)
 
-  result = ''
+  result = bytes()
   while long_value >= 256:
     div, mod = divmod(long_value, 256)
     result = chr(mod) + result
@@ -69,3 +76,12 @@ def get_bcaddress_version(strAddress):
     return ord(version)
   return None
 ###   END: Copied from https://bitcointalk.org/index.php?topic=1026.0 (public domain)
+
+if __name__ == '__main__':
+	# Test case
+	assert get_bcaddress_version('15VjRaDX9zpbA8LVnbrCAFzrVzN7ixHNsC') is 0
+	_ohai = 'o hai'.encode('ascii')
+	_tmp = b58encode(_ohai)
+	assert _tmp == 'DYB3oMS'
+	assert b58decode(_tmp, 5) == _ohai
+	print("Tests passed")
